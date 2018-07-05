475513b871030134643d9f2370f7e19d9bd8ee85
nagyrobert1990 <nagy.robert1990@gmail.com>
2018-07-05 11:08:20 +0200

    application is ready

--- ProcessNote/ProcessNote.Designer.cs
+++ ProcessNote/ProcessNote.Designer.cs
@@ -1,4 +1,7 @@
-﻿namespace ProcessNote
+﻿using System;
+using System.Windows.Forms;
+
+namespace ProcessNote
 {
     partial class ProcessNote
     {
@@ -31,11 +34,17 @@
             this.commentBox = new System.Windows.Forms.TextBox();
             this.saveButton = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.refreshButton = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.refreshSelected = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
             // 
             // commentBox
             // 
-            this.commentBox.Location = new System.Drawing.Point(908, 27);
+            this.commentBox.Location = new System.Drawing.Point(448, 239);
             this.commentBox.Multiline = true;
             this.commentBox.Name = "commentBox";
             this.commentBox.Size = new System.Drawing.Size(187, 86);
@@ -43,34 +52,81 @@
             // 
             // saveButton
             // 
-            this.saveButton.Location = new System.Drawing.Point(908, 119);
+            this.saveButton.Location = new System.Drawing.Point(448, 331);
             this.saveButton.Name = "saveButton";
             this.saveButton.Size = new System.Drawing.Size(75, 23);
             this.saveButton.TabIndex = 1;
             this.saveButton.Text = "Save";
             this.saveButton.UseVisualStyleBackColor = true;
-            this.saveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(905, 145);
+            this.label1.Location = new System.Drawing.Point(445, 357);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 2;
             this.label1.Text = "label1";
             this.label1.Visible = false;
             // 
+            // refreshButton
+            // 
+            this.refreshButton.Location = new System.Drawing.Point(12, 346);
+            this.refreshButton.Name = "refreshButton";
+            this.refreshButton.Size = new System.Drawing.Size(75, 23);
+            this.refreshButton.TabIndex = 4;
+            this.refreshButton.Text = "Refresh";
+            this.refreshButton.UseVisualStyleBackColor = true;
+            this.refreshButton.Click += new System.EventHandler(this.refreshButton_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 27);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(385, 313);
+            this.dataGridView1.TabIndex = 5;
+            this.dataGridView1.RowHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_RowHeaderMouseClick);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(448, 27);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(644, 158);
+            this.dataGridView2.TabIndex = 6;
+            this.dataGridView2.Visible = false;
+            // 
+            // refreshSelected
+            // 
+            this.refreshSelected.Location = new System.Drawing.Point(448, 191);
+            this.refreshSelected.Name = "refreshSelected";
+            this.refreshSelected.Size = new System.Drawing.Size(75, 23);
+            this.refreshSelected.TabIndex = 7;
+            this.refreshSelected.Text = "Refresh";
+            this.refreshSelected.UseVisualStyleBackColor = true;
+            this.refreshSelected.Visible = false;
+            this.refreshSelected.Click += new System.EventHandler(this.refreshSelected_Click);
+            // 
             // ProcessNote
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1121, 469);
+            this.Controls.Add(this.refreshSelected);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.refreshButton);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.saveButton);
             this.Controls.Add(this.commentBox);
             this.Name = "ProcessNote";
             this.Text = "ProcessNote";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -81,6 +137,10 @@
         private System.Windows.Forms.TextBox commentBox;
         private System.Windows.Forms.Button saveButton;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button refreshButton;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button refreshSelected;
     }
 }
 
--- ProcessNote/ProcessNote.cs
+++ ProcessNote/ProcessNote.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ProcessNote
@@ -19,17 +15,122 @@ namespace ProcessNote
             InitializeComponent();
         }
 
-        private void SaveButton_Click(object sender, EventArgs e)
+        private void saveButton_Click(object sender, EventArgs e)
         {
             if (commentBox.Text == "" || commentBox.Text.Equals(null)) {
+                label1.ForeColor = Color.Red;
                 label1.Text = "Fill the text box";
                 label1.Visible = true;
             }
             else
             {
                 note.Comment = commentBox.Text;
+                label1.ForeColor = Color.Green;
                 label1.Text = "You saved successfully!";
                 label1.Visible = true;
+                commentBox.Text = "";
+            }
+        }
+
+        private void dataGridView1_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            checkCommentBox();
+            loadSelectedProcess();
+            dataGridView2.Visible = true;
+            refreshSelected.Visible = true;
+        }
+
+        private void loadSelectedProcess()
+        {
+            int selectedId = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells[0].Value);
+            Process process = Process.GetProcessById(selectedId);
+
+            var table = new DataTable("Selected Process");
+
+            table.Columns.Add("Process ID");
+            table.Columns.Add("Process Name");
+            table.Columns.Add("CPU usage");
+            table.Columns.Add("Memory usage");
+            table.Columns.Add("Running time");
+            table.Columns.Add("Start time");
+
+            try
+            {
+                    table.Rows.Add(new object[] {
+                    process.Id,
+                    process.ProcessName,
+                    process.TotalProcessorTime,
+                    process.PagedMemorySize64,
+                    (DateTime.Now - process.StartTime),
+                    process.StartTime
+                });
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            table.AcceptChanges();
+            BindingSource source = new BindingSource();
+            source.DataSource = table;
+
+            int scroll = dataGridView2.FirstDisplayedScrollingRowIndex;
+            dataGridView2.DataSource = source;
+
+            if (scroll != -1)
+                dataGridView2.FirstDisplayedScrollingRowIndex = scroll;
+        }
+
+        private void refreshButton_Click(object sender, EventArgs e)
+        {
+            dataGridView2.Visible = false;
+            refreshSelected.Visible = false;
+
+            checkCommentBox();
+
+            var table = new DataTable("Process List");
+
+            Process[] processes = Process.GetProcesses();
+
+            table.Columns.Add("Id");
+            table.Columns.Add("Name");
+
+            for (int i = 0; i < processes.Length; ++i)
+            {
+                table.Rows.Add(new object[] { processes[i].Id, processes[i].ProcessName });
+            }
+
+            table.AcceptChanges();
+            BindingSource source = new BindingSource();
+            source.DataSource = table;
+
+            int scroll = dataGridView1.FirstDisplayedScrollingRowIndex;
+            dataGridView1.DataSource = source;
+
+            if (scroll != -1)
+                dataGridView1.FirstDisplayedScrollingRowIndex = scroll;
+        }
+
+        private void refreshSelected_Click(object sender, EventArgs e)
+        {
+            checkCommentBox();
+
+            dataGridView2.Visible = false;
+            loadSelectedProcess();
+            dataGridView2.Visible = true;
+        }
+
+        private void checkCommentBox()
+        {
+            if (!commentBox.Text.Equals(null) && !commentBox.Text.Equals(""))
+            {
+                label1.Text = "You have an unsaved comment!";
+                label1.ForeColor = Color.Red;
+                label1.Visible = true;
+            }
+            else
+            {
+                label1.Visible = false;
             }
         }
     }
